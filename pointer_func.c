3cea70467c857a9ee04cd8294cd3d01d7136f1ec
alexanderleutgeb <alex.leutgeb02@gmail.com>
2018-10-18 11:06:22 +0200

    comments addet

--- pointer_func.c
+++ pointer_func.c
@@ -5,11 +5,14 @@ void change_integers(int int_value, int* int_pointer);
 
 int main(int argc, char const *argv[]) {
   int int_value = 123; //the value of the int
-  int* int_pointer = &int_value; //the address of the int
+  int* int_pointer = *int_value;
+  /*We have to declare the int_pointer with & + a other variable because
+    we get the address of the int_value for example.
+    If we only have a number there, there will be a warning or a error*/
   print_integers(int_value,int_pointer);
   change_integers(int_value,int_pointer);
   print_integers(int_value,int_pointer);
-  
+
   return 0;
 }
 void print_integers(int int_value, int* int_pointer){
